e70e1cd684d83a52168e66144882fa2161ff6c69
sugarcontent <chuakianchiu@gmail.com>
2020-01-03 16:23:43 +0800

    modify site information

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -30,7 +30,7 @@ class Portfolio extends Component {
 
          <div className="twelve columns collapsed">
 
-            <h1>Apart from coding, here are some Personal Works on travelling.</h1>
+            <h1>Apart from coding, here are some of my Personal Works on travelling.</h1>
 
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
                 {projects}
